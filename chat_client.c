f3fdfb8c250c8f364de03d1b92cd77b866c08b46
Carlos Arriola <119271333+carlos-arriola@users.noreply.github.com>
2024-01-16 01:21:36 -0800

    Update chat_client.c

--- chat_client.c
+++ chat_client.c
@@ -1,4 +1,4 @@
-# DISCLAIMER: As stated in the README, credit to this project goes to 'Idiot Developer' on YouTube for
+# CREDIT: As stated in the README, credit to this project goes to 'Idiot Developer' on YouTube for
 # his excellent tutorial on socket programming in C with threads. My publication is merely my following of
 # his tutorial available at https://www.youtube.com/watch?v=fNerEo6Lstw
 
